2cd05e33e1721bfd24ffb06460b7f50a66bf335f
lunessaofficialth <lunessa.official.th@gmail.com>
2026-07-30 11:54:23 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+const beginButton = document.getElementById("begin-ritual");
 
+const ritualSection = document.getElementById("ritual-selection");
+
+beginButton.addEventListener("click", () => {
+
+    ritualSection.scrollIntoView({
+
+        behavior: "smooth"
+
+    });
+
+});
